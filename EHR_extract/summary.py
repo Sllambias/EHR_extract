3abdb8018c5f662694e1720d90bbc000f0079057
LlambiasMBP <llambias@live.com>
2026-04-13 10:33:35 +0200

    .

--- EHR_extract/summary.py
+++ EHR_extract/summary.py
@@ -1,10 +1,67 @@
 import hydra
+import json
+import polars as pl
 from dotenv import load_dotenv
+from EHR_extract.custom_find_functions import find_close_siblings, find_scantime_ga, find_images_and_timedeltas
 from EHR_extract.paths import get_config_path
-from EHR_extract.utils import load_table
-from omegaconf import DictConfig
+from EHR_extract.utils import (
+    filter_numeric_rows,
+    get_python_operator,
+    load_table,
+    update_population,
+)
+from omegaconf import DictConfig, OmegaConf
 
 load_dotenv()
+
+custom_functions = {
+    "find_close_siblings": find_close_siblings,
+    "find_images_and_timedeltas": find_images_and_timedeltas,
+    "find_scantime_ga": find_scantime_ga,
+}
+
+
+def get_column_distribution(column):
+    stats = {}
+    if column.dtype.is_numeric():
+        stats["mean"] = column.mean()
+        stats["max"] = column.max()
+        stats["min"] = column.min()
+        stats["NA/NULL"] = column.null_count()
+        stats["NOT NA/NULL"] = len(column) - stats["NA/NULL"]
+    else:
+        stats["unique"] = list(column.unique())
+        stats["NA/NULL"] = column.null_count()
+        stats["NOT NA/NULL"] = len(column) - stats["NA/NULL"]
+    return stats
+
+
+def summary_from_cfg(cfg):
+    all_dists = {}
+    population = set(load_table(cfg.base_population.table)[cfg.base_population.column])
+    print("Population size:", len(population))
+
+    print("\n ### Applying standard criteria ### \n")
+    for table_cfg in cfg.get("standard_criteria", []):
+        table = load_table(table_cfg.table, strict=cfg.strict)
+        all_dists[table_cfg.table] = {}
+        print(f"Table rows total: {len(table)} for table: {table_cfg.table}")
+        table = table.filter(pl.col(table_cfg.match_on).is_in(population))
+        print(f"Table rows matching population IDs: {len(table)} after filtering on {table_cfg.match_on}")
+        for criteria in table_cfg.get("criteria", []):
+            tmp_table = table.clone()
+            all_dists[table_cfg.table][criteria.column] = (
+                get_column_distribution(column=tmp_table.get_column(criteria.column)) if cfg.distribution_save_path else {}
+            )
+
+        for multicolumn_criteria in table_cfg.get("multicolumn_criteria", []):
+            tmp_table = table.clone()
+            for criterion in criteria.criteria:
+                all_dists[table_cfg.table][criteria.column] = (
+                    get_column_distribution(column=tmp_table.get_column(criteria.column)) if cfg.distribution_save_path else {}
+                )
+
+    return all_dists
 
 
 @hydra.main(
@@ -13,18 +70,11 @@ load_dotenv()
     version_base="1.2",
 )
 def main(cfg: DictConfig) -> None:
-    print(cfg)
-    print(f"### Generating table summary for {cfg.table} with head: ###")
-    print(load_table(cfg.table, n_rows=5).head())
-    print("### Loading full table ###")
-    table = load_table(cfg.table)
+    dist = summary_from_cfg(cfg)
+    print(dist)
+    with open(cfg.distribution_save_path, "w") as fp:
+        json.dump(dist, fp, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
     main()
-
-
-# %%
-
-
-# %%
