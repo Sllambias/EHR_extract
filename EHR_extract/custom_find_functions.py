0cc842a6a75aab9f1f242a892f8a64d3f1590cc9
Montgomeryyyy <maria.elk.mont@gmail.com>
2026-04-27 10:24:39 +0200

    GA_date cutoff

--- EHR_extract/custom_find_functions.py
+++ EHR_extract/custom_find_functions.py
@@ -183,3 +183,13 @@ def find_GA_weeks(table, GA_days_col, GA_weeks_col):
         (pl.col(GA_days_col).cast(pl.Int64, strict=False) / 7).alias(GA_weeks_col)
     )
     return table
+
+def find_date_at_GA(table, birth_date_col, GA_days_col, GA_number, date_col):
+    GA_difference = pl.col(GA_days_col).cast(pl.Int64, strict=False) - int(GA_number)
+    table = table.with_columns(
+        (
+            pl.col(birth_date_col).cast(pl.Date, strict=False)
+            - pl.duration(days=GA_difference)
+        ).alias(date_col)
+    )
+    return table
