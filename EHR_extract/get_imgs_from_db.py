9681b7751e030ff19a9575d8df302435b0986bb4
LlambiasMBP <llambias@live.com>
2026-05-29 20:09:35 +0200

    Update SQL query

--- EHR_extract/get_imgs_from_db.py
+++ EHR_extract/get_imgs_from_db.py
@@ -2,17 +2,20 @@ import pandas as pd
 import sqlite3
 from datetime import datetime
 
-db_path = "/projects/users/data/UCPH/DeepFetal/ultrasound/ultrasound_metadata_db.sqlite"
+db_path = "/projects/users/data/UCPH/DeepFetal/ultrasound/tables/DeepFetal_image_database_250526.sqlite"
 
 date = datetime.today().strftime("%Y-%m-%d")
 
 conn = sqlite3.connect(db_path)
 
 query = """
-    SELECT t1.file_path, t1.no_ocr_preprocessed_file_path, t1.study_date, t1.physical_delta_x, t1.physical_delta_y, t2.phair_hash
-    FROM metadata_cache t1
-    JOIN cpr_hashes t2 ON t1.file_hash = t2.xxhash
+   SELECT t1.file_path, t1.no_ocr_preprocessed_file_path, t2.phair_hash, t3.study_date,
+          t3.physical_delta_x, t3.physical_delta_y
+   FROM path_table t1
+   JOIN cpr_hashes t2 ON t1.file_hash = t2.xxhash
+   JOIN dicom_metadata_table t3 ON t1.sop_instance_uid = t3.sop_instance_uid
 """
+
 
 df = pd.read_sql_query(query, conn)
 
