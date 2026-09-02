71975e69cfcca5a4face1b7a160c87d26f7d504c
Valdemar <joli@di.ku.dk>
2026-09-02 13:04:52 +0200

    add manufacturer model name to snapshot

--- EHR_extract/get_imgs_from_db.py
+++ EHR_extract/get_imgs_from_db.py
@@ -11,7 +11,7 @@ conn = sqlite3.connect(db_path)
 query = """
    SELECT t1.file_path, t1.no_ocr_preprocessed_file_path, t2.phair_hash, t3.study_date,
           t3.physical_delta_x, t3.physical_delta_y, t3.region_location_min_x0, t3. region_location_min_y0,
-          t3.region_location_max_x1, region_location_max_y1
+          t3.region_location_max_x1, t3.region_location_max_y1, t3.manufacturer_model_name
    FROM path_table t1
    JOIN cpr_hashes t2 ON t1.file_hash = t2.xxhash
    JOIN dicom_metadata_table t3 ON t1.sop_instance_uid = t3.sop_instance_uid
